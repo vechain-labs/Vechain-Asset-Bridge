3a12c16362deb461fedffc81d405522a3b92e386
mongelly <mog.lu@vechain.org>
2021-12-23 14:55:12 +0800

    update

--- apiserve/src/activeSupportService.ts
+++ apiserve/src/activeSupportService.ts
@@ -39,19 +39,13 @@ export default class ActiveSupportServices implements IActiveSupportServices{
     }
 
     private async initBridgeEnv(){
-        const masterNode = Devkit.HDNode.fromMnemonic((environment.config.mnemonic as string).split(' '));
-        const account = masterNode.derive(5);
         const web3 = new Web3(new Web3.providers.HttpProvider(environment.config.ethereum.nodeHost));
-        web3.eth.accounts.wallet.add(account.privateKey!.toString('hex'));
-        const wallet = new SimpleWallet();
-        wallet.import(account.privateKey!.toString('hex'));
-        const driver = await Driver.connect(new SimpleNet(environment.config.vechain.nodeHost as string), wallet);
+        const driver = await Driver.connect(new SimpleNet(environment.config.vechain.nodeHost as string));
         const connex = new Framework(driver);
 
         environment.connex = connex;
         environment.web3 = web3;
         environment.contractdir = path.join(__dirname,"../../../src/SmartContracts/contracts");
-        environment.wallet = wallet;
         environment.bridgePack = false;
     }
 }
